b06ee263f2bdf3012feb89c0faa8f480348089e6
adriandpExt <adriandp.ext@gmail.com>
2024-09-13 17:07:58 +0800

    added utils icons

--- src/components/svg-icons/utils.tsx
+++ src/components/svg-icons/utils.tsx
@@ -1,1 +1,13 @@
-export type IconName = "ic_react";
+export type IconName =
+  | "ic_svl_b1"
+  | "ic_svl_b2"
+  | "ic_svl_bs1"
+  | "ic_svl_bs2"
+  | "ic_svl_g1"
+  | "ic_svl_g2"
+  | "ic_svl_gs1"
+  | "ic_svl_gs2"
+  | "ic_svl_ib"
+  | "ic_svl_ig"
+  | "ic_svl_sib"
+  | "ic_svl_sig";
