133c50e21bef6746ef396c3e5fd122271f7d2a75
virgil-ferolino <gitMaleFerolino@gmail.com>
2024-09-16 10:21:46 +0800

    fix nav bar conflict

--- src/components/nav-bar/NavBar.tsx
+++ src/components/nav-bar/NavBar.tsx
@@ -1,24 +1,22 @@
-import { useNavigate } from "react-router-dom";
 import { Sun } from "lucide-react";
 
 import { Button } from "../ui/button";
 
 import { SvgIcons } from "../svg-icons";
+import { useStore } from "@/store/useStore";
 
 import Drawer from "./Drawer";
 
 import { linkList } from "./utils";
+import { SvgIcons } from "../svg-icons";
+import { useStore } from "@/store/useStore";
 
 export const NavBar = () => {
-  const navigate = useNavigate();
-
-  const handleNavigateHome = () => {
-    navigate("/");
-  };
+  const { scrollToSection } = useStore();
   return (
-    <div className="border-b-2">
+    <div className="absolute top-0 w-full border-b-2">
       <header className="container mx-auto flex h-14 items-center justify-between px-4 py-6 transition-all">
-        <Button variant={"icon"} onClick={handleNavigateHome}>
+        <Button variant={"icon"} onClick={() => scrollToSection("howToPage")}>
           <SvgIcons name="ic_svl_gs2" size={150} />
         </Button>
 
@@ -35,7 +33,11 @@ export const NavBar = () => {
         </nav>
 
         <div className="flex items-center space-x-4">
-          <Button className="hidden bg-green-500 hover:bg-green-800 lg:inline-flex">
+          <Button
+            variant="outline"
+            className="hidden bg-green-500 hover:bg-green-800 lg:inline-flex"
+            onClick={() => scrollToSection("howToPage")}
+          >
             Android
           </Button>
           <Sun />
