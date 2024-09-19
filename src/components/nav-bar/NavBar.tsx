0e1083cf775a521634f1afe94d2185dd1b57bca9
Dennis <denisalpha777@gmail.com>
2024-09-19 10:12:14 +0800

    add i18n

--- src/components/nav-bar/NavBar.tsx
+++ src/components/nav-bar/NavBar.tsx
@@ -1,7 +1,5 @@
 import { Button } from "../ui/button";
-
 import { SvgIcons } from "../svg-icons";
-
 import {
   Select,
   SelectContent,
@@ -10,21 +8,31 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-
 import { useStore } from "@/store/useStore";
 import useScroll from "@/hooks/useScroll";
-
 import Drawer from "./Drawer";
-
 import { linkList } from "./utils";
-
 import { Separator } from "../ui/separator";
 import { Globe, Moon, Sun } from "lucide-react";
 import { useTheme } from "@/hooks/useTheme";
-
+import { useTranslation } from "react-i18next";
+import { useState } from "react";
 export const NavBar = () => {
   const { scrollToSection } = useStore();
   const { theme, setTheme } = useTheme();
+
+  const [selectedLanguage, setSelectedLanguage] = useState<string>("");
+
+  const { i18n } = useTranslation();
+
+  const handleValueChange = (value: string) => {
+    setSelectedLanguage(value);
+    handleChangeLanguage(value);
+  };
+
+  const handleChangeLanguage = (lang: string) => {
+    i18n.changeLanguage(lang);
+  };
 
   const isScroll = useScroll(window.innerHeight);
 
@@ -36,7 +44,9 @@ export const NavBar = () => {
             <Button
               key={item}
               variant="link"
-              className={`font-semibold hover:text-gray-500 hover:no-underline ${isScroll ? "text-black" : "text-white"} `}
+              className={`font-semibold hover:text-gray-500 hover:no-underline ${
+                isScroll ? "text-black" : "text-white"
+              } `}
               onClick={() => scrollToSection(id)}
             >
               {item}
@@ -52,6 +62,7 @@ export const NavBar = () => {
       </nav>
     );
   };
+
   return (
     <header
       className={`sticky top-0 z-50 mx-auto flex h-28 w-full items-center justify-between px-4 py-6 backdrop-blur-sm transition-all ${
@@ -71,18 +82,25 @@ export const NavBar = () => {
           onClick={() => setTheme(theme === "light" ? "dark" : "light")}
         >
           <Sun
-            className={`${isScroll ? "text-black" : "text-white"} h-[1.5rem] w-[1.3rem] dark:hidden`}
+            className={`${
+              isScroll ? "text-black" : "text-white"
+            } h-[1.5rem] w-[1.3rem] dark:hidden`}
           />
           <Moon
-            className={`${isScroll ? "text-black" : "text-white"} hidden h-5 w-5 dark:block`}
+            className={`${
+              isScroll ? "text-black" : "text-white"
+            } hidden h-5 w-5 dark:block`}
           />
         </Button>
 
         <Drawer />
 
-        <Select>
+        {/* Language Selection */}
+        <Select onValueChange={handleValueChange} value={selectedLanguage}>
           <SelectTrigger
-            className={`hidden w-auto items-center justify-start space-x-2 whitespace-nowrap lg:flex ${isScroll ? "text-black" : "text-white"}`}
+            className={`hidden w-auto items-center justify-start space-x-2 whitespace-nowrap lg:flex ${
+              isScroll ? "text-black" : "text-white"
+            }`}
           >
             <SelectValue
               placeholder={
@@ -96,14 +114,16 @@ export const NavBar = () => {
             className={`${isScroll ? "text-black" : "text-white"}`}
           >
             <SelectGroup>
-              <SelectItem value="chinese">
+              <SelectItem value="en">
                 <div className="flex items-center">
-                  <span>Chinese</span>
+                  <SvgIcons name="ic_uk_flag" size={30} />
+                  <span className="ml-2">En</span>
                 </div>
               </SelectItem>
-              <SelectItem value="english">
+              <SelectItem value="ph">
                 <div className="flex items-center">
-                  <span>English</span>
+                  <SvgIcons name="ic_ph_flag" size={30} />
+                  <span className="ml-2">Ph</span>
                 </div>
               </SelectItem>
             </SelectGroup>
