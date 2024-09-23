a8cf4b9018f0fd8a2c4800b326649179dca2df0b
virgil-ferolino <gitMaleFerolino@gmail.com>
2024-09-23 12:41:49 +0800

    fix products carousel

--- src/screens/home/features/Features.tsx
+++ src/screens/home/features/Features.tsx
@@ -1,5 +1,4 @@
 import { Product } from "./types";
-import { Card, CardContent } from "@/components/ui/card";
 import {
   Carousel,
   CarouselContent,
@@ -29,13 +28,13 @@ const Features = () => {
     setCurrentIndex(index);
   };
   return (
-    <section className="flex h-fit w-full flex-col place-items-center bg-[url('@/assets/bgCarousel.png')] bg-cover bg-no-repeat pb-32 pt-20">
+    <section className="flex h-fit w-full flex-col place-items-center bg-[url('@/assets/bgCarouselLight.png')] from-green-400 via-black to-green-400 bg-cover bg-no-repeat pb-32 pt-20 dark:bg-gradient-to-br">
       <h1 className="mb-14 text-center font-poppins text-5xl font-semibold text-white lg:text-[96px]">
         PRODUCTS
       </h1>
       <Carousel
         opts={{ align: "center" }}
-        className="w-[95%] lg:w-[85%]"
+        className="w-[95%] lg:h-[100%] lg:w-[90%]"
         setApi={(api) => {
           if (api !== undefined) {
             api.on("select", () =>
@@ -48,38 +47,36 @@ const Features = () => {
           {products.map((data, index) => (
             <CarouselItem
               key={index}
-              className="flex w-full justify-center lg:basis-1/3"
+              className="flex h-[50rem] w-full items-center justify-center lg:basis-1/3"
             >
-              <Card className="rounded-[2.5rem] border-x-[1.5px] border-y-2 border-b-2 border-x-green-600 border-y-green-500 shadow-inner shadow-green-700">
-                <CardContent className="flex h-[439px] w-[358px] flex-col place-content-center items-center space-y-5 rounded-[2.5rem] border-0 p-0">
-                  <div
-                    className="inherit absolute -z-50 flex h-[439px] w-[358px] rounded-[2.5rem] border-[#414141] bg-[#414141]/50 backdrop-blur-lg lg:basis-1/3"
-                    style={{
-                      clipPath: " polygon(100% 0%, 0% 0%, 0% 63%, 100% 23%)",
-                    }}
-                  />
-                  <div
-                    className="inherit absolute -z-50 h-[439px] w-[358px] rounded-[3rem] bg-green-400/20 backdrop-blur-lg"
-                    style={{
-                      clipPath: "polygon(100% 100%, 0% 100%, 0% 60%, 100% 20%)",
-                    }}
-                  />
-                  <img
-                    src={data.image}
-                    className="h-[50%] w-auto object-contain"
-                    alt="Product Image"
-                  />
-                  <h1 className="font-sans text-4xl font-semibold tracking-tighter text-white">
-                    {data.name}
-                  </h1>
-                  <Button
-                    className="w-20 rounded-3xl bg-green-500 text-black hover:bg-green-400"
-                    onClick={(e) => handleOpen(e, data)}
-                  >
-                    More
-                  </Button>
-                </CardContent>
-              </Card>
+              <div className="flex h-[50svh] w-[80svw] flex-col place-content-center items-center space-y-5 rounded-[3rem] border-0 p-0 px-5 shadow-2xl shadow-black md:h-[35rem] lg:w-[25rem]">
+                <div
+                  className="absolute -z-50 flex h-[50svh] w-[80svw] rounded-[3rem] border-x-0 border-t-2 border-green-400 bg-[#414141]/50 shadow-inner shadow-[#414141] backdrop-blur-lg md:h-[35rem] lg:w-[25rem]"
+                  style={{
+                    clipPath: "polygon(100% 0%, 0% 0%, 0% 65%, 100% 25%)",
+                  }}
+                />
+                <div
+                  className="absolute -z-50 h-[48svh] w-[80svw] rounded-[3rem] border-x-0 border-b-2 border-green-400 bg-green-400/20 shadow-inner shadow-green-400 backdrop-blur-lg md:h-[33.6rem] lg:w-[25rem]"
+                  style={{
+                    clipPath: "polygon(100% 100%, 0% 100%, 0% 60%, 100% 20%)",
+                  }}
+                />
+                <img
+                  src={data.image}
+                  className="h-[50%] w-auto object-contain"
+                  alt="Product Image"
+                />
+                <h1 className="font-sans text-4xl font-semibold tracking-tighter text-white">
+                  {data.name}
+                </h1>
+                <Button
+                  className="w-20 rounded-3xl bg-green-500 text-black hover:bg-green-400"
+                  onClick={(e) => handleOpen(e, data)}
+                >
+                  More
+                </Button>
+              </div>
             </CarouselItem>
           ))}
         </CarouselContent>
--- src/screens/home/how-to/HowTo.tsx
+++ src/screens/home/how-to/HowTo.tsx
@@ -2,11 +2,8 @@ import { useState } from "react";
 import { StepCardProps } from "./type";
 
 import { PageContainer } from "@/components";
-import HowToCard from "./components/HowToCard";
 import { Label } from "@/components/ui/label";
-import { handleLenght } from "./utils";
-import { cn } from "@/lib/utils";
-import { motion } from "framer-motion";
+import StepCard from "./components/StepCard";
 
 const stepCard: Omit<StepCardProps, "currentStep" | "onClick">[] = [
   {
@@ -38,123 +35,6 @@ const stepCard: Omit<StepCardProps, "currentStep" | "onClick">[] = [
   },
 ];
 
-const StepCard = ({
-  stepNumber,
-  title,
-  description,
-  imgClassName,
-  currentStep,
-  onClick,
-}: StepCardProps) => {
-  const isActive = currentStep === stepNumber;
-  const cardStyle = isActive ? "bg-green-900/90" : "bg-slate-700";
-  const avatarStyle = isActive
-    ? "bg-gradient-to-t from-green-500 to-green-800"
-    : "dark:bg-white dark:text-black bg-black text-white";
-
-  return (
-    <div
-      className={`flex ${stepNumber % 2 === 0 ? "gap-10" : "flex-row-reverse gap-10"} md:contents lg:gap-0`}
-      onClick={onClick} // Clicking this will toggle the active state
-    >
-      {stepNumber % 2 === 0 ? (
-        isActive ? (
-          <div
-            className="col-start-1 col-end-5 hidden w-full flex-col items-end justify-center space-y-3 lg:flex"
-            data-aos="zoom-in"
-          >
-            <Label className="font-lexendDeca text-5xl font-bold">
-              {title}
-            </Label>
-            <p className="text-end font-lexendDeca text-xl">{description}</p>
-          </div>
-        ) : (
-          <></>
-        )
-      ) : (
-        <HowToCard
-          className={cn(
-            "col-start-1 col-end-5 rotate-[-135deg] md:rotate-[45deg] lg:p-14 md:-ml-20",
-            cardStyle,
-          )}
-          imgClassName={imgClassName}
-          even={stepNumber % 2 === 0}
-        >
-          {isActive ? (
-            <motion.div
-              initial={{ scale: 0 }}
-              animate={{ scale: 1 }}
-              transition={{ duration: 0.2, ease: "easeInOut" }}
-              className="shadow-inner-large flex h-full w-full place-content-center items-center rounded-full bg-white lg:hidden"
-            >
-              <Label className="font-lexendDeca text-lg font-bold">
-                {title.toUpperCase()}
-              </Label>
-            </motion.div>
-          ) : (
-            <></>
-          )}
-        </HowToCard>
-      )}
-      <div className="relative col-start-5 col-end-6 mr-7 md:mx-auto">
-        <div
-          className={cn(
-            "flex h-full w-6 justify-center",
-            handleLenght(stepNumber).item,
-          )}
-        >
-          <div
-            className={cn("w-2 bg-slate-400", handleLenght(stepNumber).height)}
-          />
-        </div>
-        <div
-          className={cn(
-            "absolute -left-[120%] top-1/2 -mt-10 flex h-20 w-20 place-content-center items-center rounded-full font-sans text-2xl font-bold",
-            avatarStyle,
-          )}
-        >
-          {stepNumber}
-        </div>
-      </div>
-      {stepNumber % 2 === 0 ? (
-        <HowToCard
-          className={cn(
-            "col-start-6 col-end-12 rotate-[-135deg] lg:ml-20 lg:p-14 md:ml-20",
-            cardStyle,
-          )}
-          imgClassName={imgClassName}
-          even={stepNumber % 2 === 0}
-        >
-          {isActive ? (
-            <motion.div
-              initial={{ scale: 0 }}
-              animate={{ scale: 1 }}
-              transition={{ duration: 0.2, ease: "easeInOut" }}
-              className="shadow-inner-large flex h-full w-full place-content-center items-center rounded-full bg-white lg:hidden"
-            >
-              <Label className="font-lexendDeca text-lg font-bold">
-                {title.toUpperCase()}
-              </Label>
-            </motion.div>
-          ) : (
-            <></>
-          )}
-        </HowToCard>
-      ) : isActive ? (
-        <div
-          className="col-start-6 col-end-12 hidden w-full flex-col items-start justify-center space-y-3 lg:flex"
-          data-aos="zoom-in"
-        >
-          <Label className="font-lexendDeca text-5xl font-bold">{title}</Label>
-          <p className="text-start font-lexendDeca text-xl">{description}</p>
-        </div>
-      ) : (
-        <></>
-      )}
-    </div>
-  );
-};
-
 const HowTo = () => {
   const [step, setStep] = useState(1);
   return (
@@ -166,7 +46,7 @@ const HowTo = () => {
         <Label className="font-poppins text-3xl font-bold lg:text-7xl">
           HOW TO USE ICHARGE?
         </Label>
-        <div className="mx-auto flex w-[80%] md:w-[70%] grid-cols-9 flex-col p-2 md:grid">
+        <div className="mx-auto flex w-[80%] grid-cols-9 flex-col p-2 md:grid md:w-[70%]">
           {stepCard.map((card, index) => (
             <StepCard
               key={index}
