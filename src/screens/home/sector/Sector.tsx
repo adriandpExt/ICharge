ee21aa53795b6c02cfc50cc7a70f43dfeb97bfd9
virgil-ferolino <gitMaleFerolino@gmail.com>
2024-09-24 12:27:34 +0800

    fixed desktop sector and remaned features folder

--- src/screens/home/sector/Sector.tsx
+++ src/screens/home/sector/Sector.tsx
@@ -1,86 +1,126 @@
 import { PageContainer } from "@/components";
 import { Card } from "@/components/ui/card";
-import { ReactElement, useState } from "react";
+import { Label } from "@/components/ui/label";
+import { ReactElement } from "react";
 
-const sectorsDesktop = [
-  { name: "", col: 1, row: 1 },
-  { name: "", col: 1, row: 2 },
-  { name: "HOSPITALITY", col: 1, row: 3 },
-  { name: "", col: 1, row: 4 },
+interface SectorProps {
+  name: string;
+  col: number;
+  row: number;
+}
+
+const sectorsDesktop: SectorProps[] = [
   { name: "", col: 2, row: 1 },
-  { name: "TRANSPORT", col: 2, row: 2 },
-  { name: "", col: 2, row: 3 },
+  { name: "", col: 1, row: 2 },
+  { name: "", col: 1, row: 3 },
+  { name: "EVENTS", col: 2, row: 3 },
+  { name: "TRANSPORT", col: 1, row: 4 },
   { name: "", col: 2, row: 4 },
-  { name: "", col: 3, row: 1 },
-  { name: "EVENTS", col: 3, row: 2 },
-  { name: "HEALTHCARE", col: 3, row: 3 },
-  { name: "", col: 3, row: 4 },
-  { name: "", col: 4, row: 1 },
-  { name: "", col: 4, row: 2 },
-  { name: "", col: 4, row: 3 },
-  { name: "RETAIL", col: 4, row: 4 },
+  { name: "HOSPITALITY", col: 1, row: 5 },
+  { name: "HEALTHCARE", col: 2, row: 5 },
+  { name: "", col: 1, row: 6 },
+  { name: "RETAIL", col: 2, row: 6 },
+  { name: "", col: 2, row: 7 },
+  // { name: "", col: 1, row: 2 },
+  // { name: "HOSPITALITY", col: 1, row: 3 },
+  // { name: "", col: 1, row: 4 },
+  // { name: "", col: 2, row: 1 },
+  // { name: "TRANSPORT", col: 2, row: 2 },
+  // { name: "", col: 2, row: 3 },
+  // { name: "", col: 2, row: 4 },
+  // { name: "", col: 3, row: 1 },
+  // { name: "EVENTS", col: 3, row: 2 },
+  // { name: "HEALTHCARE", col: 3, row: 3 },
+  // { name: "", col: 3, row: 4 },
+  // { name: "", col: 4, row: 1 },
+  // { name: "", col: 4, row: 2 },
+  // { name: "", col: 4, row: 3 },
+  // { name: "RETAIL", col: 4, row: 4 },
 ];
 
-const sectorsMobile = [
-  { name: "", col: 2, row: 2 },
-  { name: "RETAIL", col: 1, row: 3 },
-  { name: "TRANSPORT", col: 2, row: 3 },
-  { name: "HOSPITALITY", col: 2, row: 4 },
-  { name: "EVENTS", col: 1, row: 5 },
-  { name: "HEALTHCARE", col: 2, row: 5 },
-  { name: "", col: 2, row: 6 },
-];
+// const sectorsMobile = [
+//   { name: "", col: 2, row: 2 },
+//   { name: "RETAIL", col: 1, row: 3 },
+//   { name: "TRANSPORT", col: 2, row: 3 },
+//   { name: "HOSPITALITY", col: 2, row: 4 },
+//   { name: "EVENTS", col: 1, row: 5 },
+//   { name: "HEALTHCARE", col: 2, row: 5 },
+//   { name: "", col: 2, row: 6 },
+// ];
 
 const Sector = (): ReactElement => {
-  const [hoveredSector, setHoveredSector] = useState<string | null>(null);
-
   const renderSectors = (
     sectors: { name: string; col: number; row: number }[],
-  ) =>
-    sectors.map((sector) => (
-      <Card
-        key={`${sector.name}-${sector.col}-${sector.row}`}
-        style={{
-          gridColumn: sector.col,
-          gridRow: sector.row,
-          clipPath:
-            "polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%)",
-          transform:
-            sector.row % 2 === 0 ? "translateX(-50%)" : "translateX(0)",
-          marginBottom: "-50px",
-        }}
-        className={`flex h-[200px] w-[190px] cursor-pointer items-center justify-center bg-[#033800] text-center font-staatliches text-sm text-white transition-all duration-300 lg:text-lg ${
-          hoveredSector === sector.name
-            ? "z-10 scale-110 bg-gradient-to-t from-green-500 via-green-400 to-transparent"
-            : ""
-        }`}
-        onMouseEnter={() => setHoveredSector(sector.name)}
-        onMouseLeave={() => setHoveredSector(null)}
-      >
-        {sector.name}
-      </Card>
-    ));
+  ) => {
+    const colorHex = ({ name, row, col }: SectorProps): string => {
+      if (name !== "") {
+        return "bg-[#32802E]/50 hover:bg-green-600";
+      } else if ((row === 6 && col === 1) || (row === 7 && col === 2)) {
+        return "bg-slate-700/5 shadow-inner-hex";
+      } else {
+        return "bg-slate-700/30 shadow-inner-hex";
+      }
+    };
+    const transform = (row: number): string => {
+      switch (row) {
+        case 1:
+          return "translateX(125%) translateY(0)";
+        case 2:
+          return "translateX(208.5%) translateY(-50%)";
+        case 3:
+          return "translateY(-100%) translateX(125%)";
+        case 4:
+          return "translateX(208.5%) translateY(-150%)";
+        case 5:
+          return "translateY(-200%) translateX(125%)";
+        case 6:
+          return "translateX(208.5%) translateY(-250%)";
+        case 7:
+          return "translateY(-300%) translateX(125%)";
+      }
+      return "";
+    };
+    return sectors.map((sector) => {
+      return (
+        <Card
+          key={`${sector.name}-${sector.col}-${sector.row}`}
+          style={{
+            gridColumn: sector.col,
+            gridRow: sector.row,
+            clipPath:
+              "polygon(25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%, 0% 50%)",
+            transform: transform(sector.row),
+          }}
+          className={`flex h-[200px] ${colorHex(sector)} w-[190px] items-center justify-center border-0 text-center font-staatliches text-sm text-white transition-all duration-300 dark:text-black lg:text-2xl`}
+        >
+          {sector.name}
+        </Card>
+      );
+    });
+  };
 
   return (
-    <PageContainer>
+    <PageContainer className="flex flex-col lg:h-[85svh]">
       <h1 className="pb-14 text-center font-staatliches text-6xl dark:text-white">
         Sectors
       </h1>
-
-      <Card className="flex h-full items-center justify-center border-0 pb-28 shadow-none">
+      <Label className="flex text-center text-3xl lg:hidden">
+        NOT SUPPORTED (TO BE UPDATED)
+      </Label>
+      <Card
+        className="inherit absolute mx-auto hidden w-fit translate-x-[13.5%] translate-y-[8%] border-0 shadow-none lg:grid"
+        style={{
+          gridTemplateColumns: "repeat(4, 1fr)",
+          gridTemplateRows: "repeat(4, 1fr)",
+          columnGap: 130,
+          rowGap: 10,
+        }}
+      >
         {/* Desktop layout */}
-        <Card
-          className="relative hidden gap-1 border-0 shadow-none lg:grid"
-          style={{
-            gridTemplateColumns: "repeat(4, 1fr)",
-            gridTemplateRows: "repeat(4, 1fr)",
-          }}
-        >
-          {renderSectors(sectorsDesktop)}
-        </Card>
+        {renderSectors(sectorsDesktop)}
 
         {/* Mobile layout */}
-        <Card
+        {/* <Card
           className="relative grid gap-1 border-0 shadow-none lg:hidden"
           style={{
             gridTemplateColumns: "repeat(1, 1fr)",
@@ -88,7 +128,7 @@ const Sector = (): ReactElement => {
           }}
         >
           {renderSectors(sectorsMobile)}
-        </Card>
+        </Card> */}
       </Card>
     </PageContainer>
   );
