06018b6a9ed951f18818a7307996013d120c0e1a
adriandpExt <adriandp.ext@gmail.com>
2024-09-26 15:00:34 +0800

    change navbar

--- src/components/nav-bar/utils.tsx
+++ src/components/nav-bar/utils.tsx
@@ -1,13 +1,52 @@
 import { RouteObject } from "react-router-dom";
 
+interface SubChild extends Omit<RouteObject, "children"> {
+  label: string;
+  icon: string;
+  sublabel: string;
+}
+
 interface LinkList extends Omit<RouteObject, "children"> {
   label: string;
+  subChild?: SubChild[];
 }
 
 export const linkList: LinkList[] = [
   {
     label: "Sectors",
     path: "/sectors",
+    subChild: [
+      {
+        path: "/hospitality",
+        label: "Hospitality",
+        icon: "ICON",
+        sublabel: " Lorem, ipsum dolor sit amet consectetur.",
+      },
+      {
+        path: "/transport",
+        label: "Transport",
+        icon: "ICON",
+        sublabel: " Lorem, ipsum dolor sit amet consectetur.",
+      },
+      {
+        path: "/healthcare",
+        label: "Healthcare",
+        icon: "ICON",
+        sublabel: " Lorem, ipsum dolor sit amet consectetur.",
+      },
+      {
+        path: "/events",
+        label: "Events",
+        icon: "ICON",
+        sublabel: " Lorem, ipsum dolor sit amet consectetur.",
+      },
+      {
+        path: "/retail",
+        label: "Retail",
+        icon: "ICON",
+        sublabel: " Lorem, ipsum dolor sit amet consectetur.",
+      },
+    ],
   },
   {
     label: "Partner with Us",
