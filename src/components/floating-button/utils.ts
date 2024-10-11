7738af5f027c8234955b5e54e6a72bdf8091f52f
adriandpExt <adriandp.ext@gmail.com>
2024-10-11 16:00:07 +0800

    update: public iD

--- src/components/floating-button/utils.ts
+++ src/components/floating-button/utils.ts
@@ -15,4 +15,4 @@ export const formDefaultValues = {
 
 export const serviceId = import.meta.env.VITE_SERVICE_ID;
 export const templateId = import.meta.env.VITE_TEMPLATE_ID;
-export const publicKey = import.meta.env.VITE_PUBLIC_KEY;
+export const publicKey = import.meta.env.VITE_PUBLIC_ID;
