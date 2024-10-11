ae7d39b2fa47477c753283a904ab72f2cf0023a0
adriandpExt <adriandp.ext@gmail.com>
2024-10-11 13:52:03 +0800

    update: forms component

--- src/components/forms/TextArea.tsx
+++ src/components/forms/TextArea.tsx
@@ -1,6 +1,5 @@
 import { FormFieldProps } from "./types";
-
-import { FieldValues } from "react-hook-form";
+import { FieldValues, useFormContext } from "react-hook-form";
 
 import {
   FormControl,
@@ -18,29 +17,35 @@ interface TextAreaProps<T extends FieldValues> extends FormFieldProps<T> {
   className?: string;
 }
 
-export const TextArea = <T extends FieldValues>(props: TextAreaProps<T>) => {
-  const { control, name, label, className } = props;
+export const TextArea = <T extends FieldValues>({
+  control,
+  name,
+  label,
+  className,
+}: TextAreaProps<T>) => {
   const { t } = useTranslation();
+  const { formState } = useFormContext();
+  const error = formState.errors[name];
+
   return (
     <FormField
       control={control}
       name={name}
       render={({ field }) => (
         <FormItem>
-          <FormLabel>{label}</FormLabel>
+          <FormLabel className={error && "text-red-600"}>{label}</FormLabel>
           <FormControl>
             <ShadTextArea
               {...field}
-              className={
-                (cn(
-                  "h-20 resize-none border-white bg-gray-200 placeholder:text-gray-500",
-                ),
-                className)
-              }
+              className={cn(
+                "h-20 resize-none bg-gray-200 placeholder:text-gray-500",
+                error ? "border-red-600" : "border-white",
+                className,
+              )}
               placeholder={t(LocalizationKey.common.textareaPlaceholder)}
             />
           </FormControl>
-          <FormMessage />
+          <FormMessage className="text-red-600" />
         </FormItem>
       )}
     />
