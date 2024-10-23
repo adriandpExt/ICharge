4a94b97a59dc1e5852e12ed07443f252ffb37e00
sushikansai <michaelsagun013@gmail.com>
2024-10-23 18:37:50 +0800

    feature: videoBackground optimization

--- src/screens/home/HeroSection/VideoComponent.tsx
+++ src/screens/home/HeroSection/VideoComponent.tsx
@@ -33,20 +33,15 @@ const VideoComponent = (): ReactElement => {
   }, [threshold]);
 
   return (
-    <video
-      ref={videoRef}
-      muted
-      loop
-      autoPlay={false}
-      playsInline
-      aria-label="iCharge"
-      className="absolute inset-0 h-[90vh] w-full rounded-bl-[5rem] border-b-2 border-white object-cover shadow-2xl brightness-[30%] transition-opacity duration-1000"
-    >
-      <source
-        src="https://joospower.com/wp-content/uploads/2023/10/Joos-Advert-final.mp4"
-        type="video/mp4"
-      />
-    </video>
+    <div className='absolute overflow-hidden inset-0 h-[90vh] w-full rounded-bl-[5rem] border-b-2 border-white object-cover shadow-2xl brightness-[30%] transition-opacity duration-1000'>
+            <iframe 
+                width="1920"
+                height="1080"
+                src="https://www.youtube.com/embed/E0SEzlJrFf8?autoplay=1&controls=0&mute=1&loop=1&modestbranding=1&showinfo=0&start=50&enablejsapi=1&&widgetid=3"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                className="absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]"
+                 />
+    </div>
   );
 };
 
