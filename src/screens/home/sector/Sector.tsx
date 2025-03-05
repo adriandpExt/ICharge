7386eadc3681d1e1fd381c740500885e34e86b35
cedy123 <cedrickgueta056@gmail.com>
2025-03-05 13:39:49 +0800

    convert png to jpg

--- src/screens/home/sector/Sector.tsx
+++ src/screens/home/sector/Sector.tsx
@@ -10,11 +10,11 @@ import {
   CarouselItem,
 } from "@/components/ui/carousel";
 import { Label } from "@/components/ui/label";
-import sectorHospital from "@/assets/sectors/sectorHospitality.png";
-import sectorHealthCare from "@/assets/sectors/sectorHealthcare.png";
-import sectorTransport from "@/assets/sectors/sectorTransport.png";
-import sectorEvents from "@/assets/sectors/sectorEvents.png";
-import sectorRetail from "@/assets/sectors/sectorRetail.png";
+import sectorHospital from "@/assets/sectors/sectorHospitality.jpg";
+import sectorHealthCare from "@/assets/sectors/sectorHealthcare.jpg";
+import sectorTransport from "@/assets/sectors/sectorTransport.jpg";
+import sectorEvents from "@/assets/sectors/sectorEvents.jpg";
+import sectorRetail from "@/assets/sectors/sectorRetail.jpg";
 import LocalizationKey from "@/i18n/key";
 
 const Sector = (): ReactElement => {
