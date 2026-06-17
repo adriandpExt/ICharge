6cc17b38bab1184432a1f4824ddc87f8affeaf6a
adriandpExt <152352852+adriandpExt@users.noreply.github.com>
2026-06-17 11:15:55 +0800

    Remove unused imports from vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
-import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
 
 // https://vitejs.dev/config/
 export default defineConfig({
